100f8d1292dd3d99b096c47a57889d16150d4675
Ascend96 <mmartens28@yahoo.com>
2020-10-25 20:39:53 -0500

    Asks for Animal - takes user input for animals name - takes user input for what the animal says

---
+++ src/us/mattgreen/AskForAnimal.java
@@ -0,0 +1,37 @@
+package us.mattgreen;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AskForAnimal implements Talkable{
+    Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String talk() {
+        System.out.println("Enter what the animal says");
+        String input;
+        do{
+            input = scanner.nextLine();
+            if(input.length() <= 2){
+                System.out.println("Invalid response, must be more than 2 characters please try again.");
+            }
+        } while(input.length() <= 2);
+        return input;
+    }
+
+    @Override
+    public String getName() {
+            System.out.println("Enter the type of animal");
+            String input;
+           do{ input = scanner.nextLine();
+           if(input.length() < 3){
+               System.out.println("Invalid response, must be more than 2 characters please try again");
+           }
+           }
+           while(input.length() < 3);
+
+
+            return input;
+
+    }
+}
